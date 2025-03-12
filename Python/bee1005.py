06cbd03e7bff71002492fea43241f6cc2ee629e8
fernandesITN <fernandesitsnext@gmail.com>
2025-03-12 12:17:57 -0300

    up

---
+++ Python/bee1005.py
@@ -0,0 +1,6 @@
+nota1 = float(input())
+nota2 = float(input())
+
+total = (nota1 * 3.5 + nota2 * 7.5)/11
+
+print(f"MEDIA = {total:.5f}")
---
+++ Python/bee1008.py
@@ -0,0 +1,7 @@
+numero = int(input())
+numero_horas = int(input())
+valor_hora = float(input())
+
+total = numero_horas * valor_hora
+
+print(f"NUMBER = {numero}\nSALARY = U${total:.2f}")
