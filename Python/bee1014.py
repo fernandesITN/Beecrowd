b6eda6b31ae0837e31b9da7030ff77fc62556e63
fernandesITN <fernandesitsnext@gmail.com>
2025-03-13 07:35:31 -0300

    up

---
+++ Python/bee1014.py
@@ -0,0 +1,7 @@
+dt_perc = int(input())
+gt_total = float(input())
+
+
+consumo_medio = dt_perc / gt_total
+
+print(f"{consumo_medio:.3f} km/l")
---
+++ Python/bee1017.py
@@ -0,0 +1,8 @@
+tempo_gasto = int(input())
+velocidade_media = int(input())
+
+calc = tempo_gasto * velocidade_media
+total = calc /12
+
+print(f"{total:.3f}")
+
